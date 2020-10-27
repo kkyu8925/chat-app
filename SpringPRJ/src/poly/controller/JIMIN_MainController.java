49b8910220cad593d53b8a1c1f0cec92f7123a97
hamjimin <hua4187@naver.com>
2020-10-27 12:43:41 +0900

    jimin test

---
+++ SpringPRJ/src/poly/controller/JIMIN_MainController.java
@@ -0,0 +1,43 @@
+package poly.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class JIMIN_MainController {
+	
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@RequestMapping(value="index")
+	public String Index() {
+		log.info(this.getClass());
+		
+		return "/index";
+	}
+	
+	@RequestMapping(value="friends")
+	public String friends() {
+		log.info(this.getClass());
+		
+		return "/friends";
+	}
+	
+	@RequestMapping(value="chat")
+	public String chat() {
+		log.info(this.getClass());
+		
+		return "/chat";
+	}
+	
+	@RequestMapping(value="settings")
+	public String settings() {
+		log.info(this.getClass());
+		
+		return "/settings";
+	}
+	
+	
+			
+}
